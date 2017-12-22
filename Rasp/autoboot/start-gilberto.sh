d1ad136e9ab8e897e0758ec02b2232eaf6d73ea2
uj <aulien.jbadji@gmail.com>
2017-12-22 13:19:54 +0100

    add github ids

--- Rasp/autoboot/start-gilberto.sh
+++ Rasp/autoboot/start-gilberto.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /home/pi/AVAST/Rasp/sensors/
+git config user.name 'uj'
+git config user.email 'sudo.screenlife@gmail.com'
 git pull origin master
 . /home/pi/AVAST/Rasp/sensors/bin/activate
 python gilberto.py
